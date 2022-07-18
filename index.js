3356d0a1b73729fbf080673b1b6c02c70efec60f
Alma Perez <aperez32@student.scf.edu>
2022-07-18 02:32:37 -0400

    CONTROL FLOW LAB DONE

--- index.js
+++ index.js
@@ -1,23 +1,75 @@
-function scuberGreetingForFeet(ride){
-  if (ride <=400) {
-  return ('This one is on me!')
- }
- else if (ride > 2500) {
- return ('No can do.')
-}
-else (ride > 2000) ;
-  return ('I will gladly take your thirty bucks.') 
-}
+// practice 1
+//function scuberGreetingForFeet(ride){
+//   if (ride <=400) {
+//   return ('This one is on me!')
+//  }
+//  else if (ride > 2500) {
+//  return ('No can do.')
+// }
+// else (ride > 2000) ;
+//   return ('I will gladly take your thirty bucks.') 
+// }
 
+
+// function ternaryCheckCity(city){
+// return (city ===  "NYC" ? "Ok, sounds good." : 'No go.' )
+// }
+
+// function switchOnCharmFromTip(tip){
+//   switch (tip) { 
+//     case "generous": return "Thank you so much."
+//     case "not as generous": return "Thank you."
+//     default: return "Bye."
+//   }
+// 
+
+// practice 2
+// gitfunction scuberGreetingForFeet(ride){
+//    if  (ride <= 400){
+//     return ('This one is on me!')
+//    }
+//    else if (ride>2500){
+//     return ('No can do.')
+//    }
+//    else  (ride > 2000)
+//   return ('I will gladly take your thirty bucks.')
+// }
+
+// function ternaryCheckCity(city){
+//   return ( city === "NYC" ? "Ok, sounds good." : "No go." )
+// }
+
+// function switchOnCharmFromTip(tip){
+//   switch (tip) {
+//     case 'generous': return ( 'Thank you so much.')
+//     case 'not as generous': return ('Thank you.')
+//     default: return 'Bye.'
+
+//   }
+  
+// }
+function scuberGreetingForFeet(ride){
+  if (ride<400) {
+    return ('This one is on me!')
+  }
+  else if (ride>2500) {
+    return ('No can do.')
+  }
+  else (ride >2000) 
+    return ('I will gladly take your thirty bucks.')
+  
+}
 
 function ternaryCheckCity(city){
-return (city ===  "NYC" ? "Ok, sounds good." : 'No go.' )
+  return(city === "NYC" ? "Ok, sounds good." : "No go." )
+
 }
 
-function switchOnCharmFromTip(tip){
-  switch (tip) { 
-    case "generous": return "Thank you so much."
-    case "not as generous": return "Thank you."
-    default: return "Bye."
-  }
+function switchOnCharmFromTip(tip) {
+ switch (tip) {
+  case 'generous': return ('Thank you so much.')
+  case 'not as generous': return ('Thank you.')
+  default: return 'Bye.'
+
+}
 }
